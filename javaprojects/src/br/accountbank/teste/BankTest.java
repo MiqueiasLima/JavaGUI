983f37dcbc4b74a7e5833041e9cfd9cd5aa1828c
MiqueiasLima <miqueiaslima@outlook.com>
2024-01-13 00:12:29 -0300

    Adicionando interface gráfica na Tela de Conta

--- javaprojects/src/br/accountbank/teste/BankTest.java
+++ javaprojects/src/br/accountbank/teste/BankTest.java
@@ -1,6 +1,23 @@
 package br.accountbank.teste;
 
+import javax.swing.*;
+
 public class BankTest {
+
+    private JLabel agenciaLbl;
+    private JTextField agenciaTxtField;
+    private JLabel numeroLbl;
+    private JTextField numeroTxtField;
+    private JLabel titularLbl;
+    private JTextField textField1;
+    private javax.swing.JPanel JPanel;
+    private JLabel saldoLbl;
+    private JTextField saldoTxtField;
+    private JButton criarContaBtn;
+    private JLabel valorLbl;
+    private JTextField textField2;
+    private JButton btnSacar;
+    private JButton btnDepositar;
 
     public BankTest(){
 
